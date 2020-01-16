a2b8b9b95fec6178b1dfbc9b8e9734604b43a27f
Eric Penn <ep00959@georgiasouthern.edu>
2020-01-16 09:18:15 -0500

    npm install/remove node modules

---
+++ client/build/precache-manifest.16d1b85c65f55acaf75b3e6356e01b5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef689378c64134589a1b8aba6c4a8a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35618d31ee5e8a75ce43",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "35618d31ee5e8a75ce43",
+    "url": "/static/js/main.29c6dfd6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
