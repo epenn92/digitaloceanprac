ca83157f4142264f0c43cffca5d189957a118522
Eric Penn <ep00959@georgiasouthern.edu>
2020-01-22 11:31:25 -0500

    adding css styles

--- client/src/App.js
+++ client/src/App.js
@@ -18,50 +18,64 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Router>
-          <div>
-            <Switch>
-              <Route exact path="/" component={Homepage} />
-              <Route exact path="/champion" component={Champion} />
-              <Route exact path="/champion/new" component={NewChampion} />
-              <Route exact path="/champion/edit/:championId" component={UpdateChampion} />
-              <Route exact path="/champion/:championId" component={SingleChampion} />
-              <Route exact path="/champion/items/:championId" component={BuildPage} />
-              <Route exact path="/build" component={AllBuilds} />
-              <Route exact path="/build/:buildId" component={SingleBuild} />
-              <Route exact path="/item" component={AllItems} />
-              <Route exact path="/item/new" component={NewItem} />
-              <Route exact path="/item/edit/:itemId" component={UpdateItem} />
-              <Route exact path="/item/:itemId" component={SingleItem} />
+        <div className='siteContainer'>
+          <Router>
+            <div className='Routes'>
+              <Switch>
+                <Route exact path="/" component={Homepage} />
+                <Route exact path="/champion" component={Champion} />
+                <Route exact path="/champion/new" component={NewChampion} />
+                <Route exact path="/champion/edit/:championId" component={UpdateChampion} />
+                <Route exact path="/champion/:championId" component={SingleChampion} />
+                <Route exact path="/champion/items/:championId" component={BuildPage} />
+                <Route exact path="/build" component={AllBuilds} />
+                <Route exact path="/build/:buildId" component={SingleBuild} />
+                <Route exact path="/item" component={AllItems} />
+                <Route exact path="/item/new" component={NewItem} />
+                <Route exact path="/item/edit/:itemId" component={UpdateItem} />
+                <Route exact path="/item/:itemId" component={SingleItem} />
+              </Switch>
+            </div>
+          </Router>
+          <div className="wrap">
+            <nav className="navbar">
+              <ul className="navbarOptions">
 
-              <div class="wrap">
-                <nav class="navbar">
-                  <ul class="navbarOptions">
-                    <li>
-                      <a class="brand" href="/">
-                        <img src="https://github.com/epenn92/Project-2/blob/master/images/brand.png?raw=true">
-              </a></li>
-                      <li><a href="/">Home</a></li>
-                      <li><a href="/restaurant">Restaurants</a></li>
-                      <li><a href="/menu">Menus</a></li>
-                      <li><a href="/item">Food Items</a></li>
-                      <li><a href="#">Top Rated</a></li>
-                      <li><a href="#">Contact Us</a></li>
-              
+                <li><a href="/">Home</a></li>
+                <li><a href="/champion">All Champions</a></li>
+                <li><a href="/item">All Items</a></li>
+                <li><a href="/build">Your Builds</a></li>
+
+
               </ul>
             </nav>
 
-            </div>
-
-            </Switch>
-
-            
           </div>
-        </Router>
 
 
-          );
-        }
-      }
-      
-      export default App;
+
+
+        </div>
+        <footer id='footer'>
+          <nav className="footerNavbar">
+            <ul className="footerNavbarOptions">
+              <li><p className="thanks">Thanks for visiting</p></li>
+              <li className="socialMedia">
+                <button className="icon"><a href="" className="fa fa-twitter"></a></button>
+                <button className="icon"><a href="" className="fa fa-facebook"></a></button>
+                <button className="icon"><a href="" className="fa fa-linkedin"></a></button>
+                <button className="icon"><a href="" className="fa fa-github"></a></button>
+                <button className="icon"><a href="" className="fa fa-rss"></a></button>
+              </li>
+              <li><p className="followUs">Follow us</p></li>
+            </ul>
+          </nav>
+        </footer>
+      </div>
+
+
+    );
+  }
+}
+
+export default App;
